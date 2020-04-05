24495764c43d6b00958e6aa0e99ec23d762acc89
JAS-KHEHRA <52291862+JAS-KHEHRA@users.noreply.github.com>
2020-04-05 19:21:20 +0530

    Add files via upload

---
+++ kadane algo(max_suba_rray).cpp
@@ -0,0 +1,29 @@
+//#jas_khehra
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+
+	#ifndef ONLINE_JUDGE
+	freopen("input.txt", "r" , stdin);
+    freopen("output.txt", "w" , stdout);
+    #endif
+    int n;
+    cin>>n;
+    std::vector<int> v(n);
+    for(int i=0;i<n;i++)
+    {
+    	cin>>v[i];
+    } 
+    int ans,a=0;
+    for(int i:v)
+    {
+    	a+=i;
+    	ans=max(a,ans);
+    	a=max(a,0);
+
+    }
+    cout<<ans;
+    return 0;
+}
+ 
